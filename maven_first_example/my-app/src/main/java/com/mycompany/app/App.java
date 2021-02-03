c11d6c4e181abcc99429976460cd9cf83deaba88
root <root@bhavinshahpatel.ttwtokdcop5ujjczxga2j5oqad.gx.internal.cloudapp.net>
2021-02-03 16:23:45 +0000

    version2 for project

--- maven_first_example/my-app/src/main/java/com/mycompany/app/App.java
+++ maven_first_example/my-app/src/main/java/com/mycompany/app/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "Hello World! from Webhook" );
+        System.out.println( "Hello World! from my final project " );
     }
 }
